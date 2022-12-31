385c80733936b72866a5d3275650c3dc52735b38
maulanasopian <maulanasopian12@gmail.com>
2022-12-31 23:18:53 +0700

    fix

--- koneksi.js
+++ koneksi.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host:'localhost',
+    host:'containers-us-west-30.railway.app',
     user:'root',
-    password:'',
-    database:'mydb'
+    password:'P8BLjmauqeQHe0joN2dA',
+    database:'railway'
 })
-
+// const conn = mysql.createConnection('mysql://root:P8BLjmauqeQHe0joN2dA@containers-us-west-30.railway.app:6542/railway');
 conn.connect((err)=>{
     if(err) throw err;
     console.log("terkoneksi");
