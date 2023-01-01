38d60e97263c0fc107a5850f8c58d5976410411d
maulanasopian <maulanasopian12@gmail.com>
2023-01-01 11:32:54 +0700

    BUILD

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 let routes = require('./routes');
 routes(app);
-app.listen(5000, () => {
+const PORT = 5000;
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server started on port`);
 });
 
